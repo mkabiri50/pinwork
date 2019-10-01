47763b8d4b91477b3fbad50f7709bfc9ff181b99
mahdi kabiri <mkabiri50@yahoo.com>
2019-10-02 01:09:31 +0330

    middnaight commit

--- src/Components/Sections/BestStuff/BestStuff.js
+++ src/Components/Sections/BestStuff/BestStuff.js
@@ -3,13 +3,13 @@ import Scs from '../../StuffCarts/Scs/Scs';
 const BestStuff =()=>{
     return(
         <div style={{ backgroundColor: 'white', width: '100%', height: 500, position: 'relative',  textAlign: 'center', }}>
-        <p style={{ fontSize: 24, lineHeight: ' 200%', paddingTop: 50 }}>
+        <p style={{ fontSize: 24, lineHeight: ' 200%', paddingTop:10 }}>
           با بهترین نیرو های متخصص ما آشنا شوید
      </p>
-        <p>
+        <p style={{ fontSize: 16, lineHeight: ' 200%', paddingBottom:10 }} >
           تیییتی ححثبننب بنبنب بکه لازم است برای شرایت مئرد نیاز و کاربرد های متنوع با هدف بهبود شرایط کاربردی تیتتیینی نننیییمی ننینینینن ننیننیمم ننینینین ننینیین
      </p>
-        <div Style={{ margin: 100 }}>
+        <div>
           <Scs />
         </div>
       </div>
--- src/Components/Sections/Questions/Question.js
+++ src/Components/Sections/Questions/Question.js
@@ -6,12 +6,12 @@ import Buttom from '../../Ui/Button/Button';
 class Question extends Component {
     state = {
         q1: {
-            question: 'سوالات متداول کارواش سیار در محل',
+            question: 'سوال اول: سلام حالت چطوره؟',
             answer: 'تیتیتی یی تیهی هیتهی یتهتیه یتهیتهی یهتیه یهیهتی هتهیتهی یهتیهی هی یی ی یی ی ییی یییی ییی یییی ی ',
             show: false,
         },
         q2: {
-            question: 'سوالات متداول کارواش سیار در محل',
+            question: 'سوال دوم : آب حوضم خالی میکنید؟',
             answer: 'تیتیتی یی تیهی هیتهی یتهتیه یتهیتهی یهتیه یهیهتی هتهیتهی یهتیهی ییی ی  ی ی ی یی ی یی ی  یییی یییی یییی یییی یییی یییی ییی  ییی یییی یییی یییی ییی هی یی ی یی ی ییی یییی ییی یییی ی ',
             show: false,
         },
@@ -46,40 +46,32 @@ class Question extends Component {
         }
         return (
             <div>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#fff', width: '100%', height: 500, position: 'relative',  paddingTop: 40 }}>
-                
-                        <p style={{ fontSize: 24, fontWeight: 'bold' }}>سوالات متداول کارواش سیار در محل</p>
-               
+                <div className='Qcontainer '>
+
+                    <p style={{ fontSize: 24, fontWeight: 'bold' }}>سوالات متداول کارواش سیار در محل</p>
+
                     <div className='Qcontent'>
                         <p style={{ fontSize: 16 }}>تیتیتی یی تیهی هیتهی یتهتیه یتهیتهی یهتیه یهیهتی هتهیتهی یهتیهی هیتهی یهتیهی هیهیه یهایه ییهییه هیهیی هیتتی اهیاهیییی ی یییی ی ی ی یی یی یییی یییی یییی ثثث ثثث اااا ت تتتتت تتتت  تتتتت  تیت تیتی هتیختی </p>
                     </div>
 
-                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+                    <div style={{width:800 ,  paddingBottom: 20 , textAlign:'right'}}>
                         <p className="App-intro">
-                            <button className={['QuestionBtn', backBtnImage].join(' ')} onClick={() => this.handleClick('q1')}>سوال اول: سلام حالت چطوره؟</button>
+                            <button className={['QuestionBtn', backBtnImage].join(' ')} onClick={() => this.handleClick('q1')}>{this.state.q1.question}</button>
                         </p>
-                        <ToggleDisplay show={this.state.q1.show}>
-                            <p style={{ width: 800, paddingBottom: 20 }}>
+                        <ToggleDisplay show={this.state.q1.show}>  
                                 بله خیلی متشکر خوبم اصلا از این بهتر نمیشود نییی ینی یمیمی یمیی یئمیی ییمی ییمیی ییمدی ییمیدی نیدین یییحنیی یمینی یدیمیی یندی یدیدی
-                        </p>
                         </ToggleDisplay>
-
                         <p className="App-intro">
-                            <button className={['QuestionBtn', backBtnImage].join(' ')} onClick={() => this.handleClick('q2')}>سوال اول: سلام حالت چطوره؟</button>
+                            <button className={['QuestionBtn', backBtnImage].join(' ')} onClick={() => this.handleClick('q2')}>{this.state.q2.question}</button>
                         </p>
-                        <ToggleDisplay show={this.state.q2.show}>
-                            <p style={{ width: 800, paddingBottom: 20 }}>
+                        <ToggleDisplay show={this.state.q2.show}>    
                                 بله خیلی متشکر خوبم اصلا از این بهتر نمیشود نییی ینی یمیمی یمیی یئمیی ییمی ییمیی ییمدی ییمیدی نیدین یییحنیی یمینی یدیمیی یندی یدیدی
-                     </p>
                         </ToggleDisplay>
                         <p className="App-intro">
                             <button className={['QuestionBtn', backBtnImage].join(' ')} onClick={() => this.handleClick('q3')}>سوال اول: سلام حالت چطوره؟</button>
                         </p>
-                        <ToggleDisplay show={this.state.q3.show}>
-                            <p style={{ width: 800, paddingBottom: 20 }}>
+                        <ToggleDisplay show={this.state.q3.show}>    
                                 بله خیلی متشکر خوبم اصلا از این بهتر نستسسسنخ ن خننخ نت ختت خ تخ تخنیختیخمیشود نییی ینی یمیمی یمیی یئمیی یییی یی  ی ی یی یی ی یی ی  ی یی ی ی ی ی ییمی ییمیی ییمدی ییمیدی نید
-                        </p>
-
                         </ToggleDisplay>
                     </div>
                     <Buttom btnType='info'>سوالات بیشتر</Buttom>
